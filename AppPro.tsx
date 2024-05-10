56b84a06492ad3106014313d17c9c57ada73b0e2
Alivin Shiva <alivin612003@gmail.com>
2024-05-10 14:11:37 +0530

    basic

---
+++ AppPro.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    useColorScheme
+} from 'react-native';
+
+function AppPro(): JSX.Element {
+    const isDarkMode = useColorScheme() === "dark"
+    return (
+        <View style={styles.container}>
+            <Text style ={isDarkMode ? styles.whiteText: styles.blackText}>
+                hello app user
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent: 'center'
+    },
+    whiteText:{
+        color: '#ffffff'
+    },
+    blackText:{
+        color:'#000000'
+    }
+})
+
+export default AppPro;
--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import App from './AppPro';
 import {name as appName} from './app.json';
+import AppPro from './AppPro';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppPro);
